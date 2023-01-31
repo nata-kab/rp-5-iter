f3303706a74b96bf64dfff6985c24f4049a8e995
Amadriella <nconnect1789@gmail.com>
2023-01-31 18:36:29 +0100

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ipAddresses from "./axios/ipAddresses";
 import getApiDataAxios from "./axios/getApiDataAxios";
+import "./App.css";
 
 function App() {
   console.log(getApiDataAxios(ipAddresses));
-  return <h1>Exercise 5</h1>;
+  return <h1 className="title">Exercise 5</h1>;
 }
 
 export default App;
