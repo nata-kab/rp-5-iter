0398914909684fc706d041ec85cc302f5eebba83
Amadriella <nconnect1789@gmail.com>
2023-02-01 10:46:11 +0100

    remove App test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-it(`renders the landing page`, () => {
-  render(<App />);
-
-  expect(screen.getByText("Exercise 5")).toBeInTheDocument();
-  expect(1).toEqual(1);
-});
